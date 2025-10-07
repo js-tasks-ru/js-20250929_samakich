d0c95bb361ecb3a8047bfe3acebb39c209db751e
Samakich <samakich@mail.ru>
2025-10-07 10:40:16 +0400

    Задача 3-1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrFromPath = path.split('.');
 
+  return function (obj) {
+
+    for (const item of arrFromPath) {
+      if (!Object.hasOwn(obj, item)) {
+        return;
+      }
+      obj = obj[item];
+    }
+
+    return obj;
+  };
 }

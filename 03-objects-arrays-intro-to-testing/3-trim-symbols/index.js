d5afc9b5f0b262b097fb1c4ddb8968b834d42f2a
Samakich <samakich@mail.ru>
2025-10-08 16:13:41 +0400

    Задача 3-3, плюс выполнил рекомендацию по комменту к задаче 3-2

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let result = '';
+  let count = 0;
 
+  if (size === undefined) {
+    return string;
+  }
+  if (size === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    if (i > 0 && string[i] === string[i - 1]) {
+      count++;
+    }
+    else {
+      count = 0;
+    }
+
+    if (count <= size - 1) {
+      result += string[i];
+    }
+  }
+
+  return result;
 }

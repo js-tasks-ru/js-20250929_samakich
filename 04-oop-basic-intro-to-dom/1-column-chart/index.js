b42701f66b96fa64e06aa5fae13e60fd57a28caf
Samakich <samakich@mail.ru>
2025-10-13 09:43:48 +0400

    модуль 4

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,90 @@
 export default class ColumnChart {
+  element;
+  chartHeight = 50;
 
+  constructor(props = {}) {
+    const {
+      data = [],
+      label = "",
+      value = 0,
+      link = '',
+      formatHeading = (value) => value,
+    } = props;
+
+    this.data = data;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.data = data;
+    this.formatHeading = formatHeading;
+
+    this.element = this.createElement();
+  }
+
+  createLinkTemplate() {
+    return this.link
+      ? `<a class="column-chart__link" href="${this.link}">View all</a>`
+      : '';
+  }
+
+  createChartTemplate() {
+    return this.getColumnProps(this.data).map(({value, percent}) => (
+      `<div style='--value: ${value}' data-tooltip='${percent}'></div>`
+    )).join('');
+  }
+
+  getColumnProps(data) {
+    const maxValue = Math.max(...data);
+    const scale = 50 / maxValue;
+
+    return data.map(item => {
+      return {
+        percent: (item / maxValue * 100).toFixed(0) + '%',
+        value: String(Math.floor(item * scale))
+      };
+    });
+  }
+
+  createTemplate() {
+    return `
+      <div class="column-chart" style="--chart-height: 50">
+        <div class="column-chart__title">
+          ${this.label}
+          ${this.createLinkTemplate()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">
+            ${this.formatHeading(this.value)}
+          </div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.createChartTemplate()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  createElement() {
+    const element = document.createElement("div");
+
+    element.innerHTML = this.createTemplate();
+
+    const firstElementChild = element.firstElementChild;
+    firstElementChild.classList.add('column-chart_loading');
+
+    return firstElementChild;
+  }
+
+  update(newData) {
+    this.data = newData;
+    this.element.innerHTML = this.createTemplate();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
